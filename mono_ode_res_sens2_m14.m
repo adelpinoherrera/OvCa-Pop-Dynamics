a3bdd024baac28969155a51b7575bf48db074a52
adelpinoherrera <92533365+adelpinoherrera@users.noreply.github.com>
2026-07-22 12:06:10 -0400

    Delete mono_ode_res_sens2_m14.m

--- mono_ode_res_sens2_m14.m
+++
@@ -1,6 +0,0 @@
-function dRdt = mono_ode_res_sens2_m14(t, R, rR, K, alphaR, IC50R, ce)
-    D_t = constant_D_schedule(t, ce);
-    kill_R = (D_t) ./ (IC50R + D_t);
-    alpha_tR = alphaR * t;
-    dRdt = rR * R * (1 - R / K) - alpha_tR * kill_R * R;
-end
