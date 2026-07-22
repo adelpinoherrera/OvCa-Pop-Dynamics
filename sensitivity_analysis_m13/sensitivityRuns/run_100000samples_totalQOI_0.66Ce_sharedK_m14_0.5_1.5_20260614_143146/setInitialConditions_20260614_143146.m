8eeed351b7b408594194e183c1a1f86beb3eb33c
adelpinoherrera <92533365+adelpinoherrera@users.noreply.github.com>
2026-07-22 16:54:23 -0400

    Delete sensitivity_analysis_m13 directory

--- sensitivity_analysis_m13/sensitivityRuns/run_100000samples_totalQOI_0.66Ce_sharedK_m14_0.5_1.5_20260614_143146/setInitialConditions_20260614_143146.m
+++
@@ -1,9 +0,0 @@
-function IC = setInitialConditions(p) %set initial conditions of state variables
-    S0 = p.S0;
-    R0 = p.R0;
-    T0 = S0 + R0;
-
-    IC.S0 = S0;
-    IC.R0 = R0;
-    IC.T0 = T0; 
-end
--- sensitivity_analysis_m13/updatePars.m
+++
@@ -1,5 +0,0 @@
-function p = updatePars(p, parsName, parsValue)
-for ii = 1:length(parsName)
-p.(parsName{ii}) = parsValue(ii);
-end
-end
