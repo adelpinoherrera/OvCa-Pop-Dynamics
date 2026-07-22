841aa871fd82c3c3965a0c8bf72f9a8019fd28b1
adelpinoherrera <92533365+adelpinoherrera@users.noreply.github.com>
2026-07-22 12:08:29 -0400

    Delete simulate_treat_for_dataset_withConstantD_add_only_n.m

--- simulate_treat_for_dataset_withConstantD_add_only_n.m
+++
@@ -1,7 +0,0 @@
-function S_model = simulate_treat_for_dataset_withConstantD_add_only_n(n, t_data, S0, ce, rS, KS, IC50)
-    tspan = [min(t_data), max(t_data)];
-    ode_fun = @(t, S) treated_ode_withConstantD_add_only_n(t, S, n, ce, rS, KS, IC50);
-    opts = odeset('RelTol',1e-8, 'AbsTol',1e-10);
-    [t_sol, S_sol] = ode15s(ode_fun, tspan, S0, opts);
-    S_model = interp1(t_sol, S_sol, t_data, 'pchip');
-end
