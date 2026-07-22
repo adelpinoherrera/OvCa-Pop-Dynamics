ce5e2ce6781b828ec20a7612b8bb6970d24b0aa4
adelpinoherrera <92533365+adelpinoherrera@users.noreply.github.com>
2026-07-22 16:53:55 -0400

    Delete clean_parameterization directory

--- clean_parameterization/simulate_mono_res_sens2_m14.m
+++
@@ -1,6 +0,0 @@
-function R_model = simulate_mono_res_sens2_m14(t_data, R0, rR, K, alphaR, IC50R, ce)
-    ode_fun = @(t,R) mono_ode_res_sens2_m14(t, R, rR, K, alphaR, IC50R, ce);
-    opts = odeset('RelTol',1e-8, 'AbsTol',1e-10);
-    [t_sol, R_sol] = ode15s(ode_fun, [min(t_data), max(t_data)], R0, opts);
-    R_model = interp1(t_sol, R_sol, t_data, 'pchip');
-end
--- clean_parameterization/simulate_mono_sens2_m14.m
+++
@@ -1,6 +0,0 @@
-function S_model = simulate_mono_sens2_m14(t_data, S0, rS, K, alphaS, IC50S, ce)
-    ode_fun = @(t,S) mono_ode_sens2_m14(t, S, rS, K, alphaS, IC50S, ce);
-    opts = odeset('RelTol',1e-8, 'AbsTol',1e-10);
-    [t_sol, S_sol] = ode15s(ode_fun, [min(t_data), max(t_data)], S0, opts);
-    S_model = interp1(t_sol, S_sol, t_data, 'pchip');
-end
